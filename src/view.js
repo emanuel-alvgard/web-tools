790438a19198148815b8036615ea602ba7a69ddf
emanuel-alvgard <46920077+emanuel-alvgard@users.noreply.github.com>
2021-08-30 21:40:26 +0200

    Add files via upload

---
+++ src/view.js
@@ -0,0 +1,26 @@
+// SOA
+let built = [0, 0, 0];
+let parent = [0, 0, 1];
+
+// AOS
+let elements = [
+    ["div", "id=0", ""], // this is the pre existing root element
+    ["div", "id=1", "Hello!"],
+    ["div", "id=2", "World!"],
+    ["div", "id=3", "World!"],
+]
+
+let html = "";
+
+
+function add(string, attributes, content, ) {
+    let tag = []
+    tag.push();
+}
+
+function build() {
+
+    for (let i = 0; i < parent.length; i ++) {
+
+    }
+}
