d1e4b8c3164539545c16ac3f56331222d15991b3
emanuel-alvgard <46920077+emanuel-alvgard@users.noreply.github.com>
2021-09-21 14:19:50 +0200

    Update and rename util.js to parsing.js

--- src/micro_service_test/parsing.js
+++ src/micro_service_test/parsing.js
@@ -26,23 +26,28 @@ function upper(data) {
 }
 
 function symbol(data, symbol) {
+
     
 }
 // symbol(data, '|');
 
 
+
+
+
+
 // STRING
-function static(data, string) {
+function static_string(data, string) {
     
     let i = 0;
-    let target_len = data.target.length;
-    let string_len = data.string.length;
-    while (i < target_len) {
-        if (data.pointer >= string_len) { 
+    let string_len = string.length;
+    let data_len = data.string.length;
+    while (i < string_len) {
+        if (data.pointer >= data_len) { 
             data.pointer -= i;
             return false;
         }
-        if (data.string[data.pointer] !== data.target[i]) { 
+        if (data.string[data.pointer] !== string[i]) { 
             data.pointer -= i;
             return false; 
         }
@@ -55,7 +60,7 @@ function static(data, string) {
 }
 
 
-function dynamic(data) {
+function dynamic_string(data) {
     
 }
 
@@ -65,8 +70,21 @@ function dynamic(data) {
 // dynamic_subset(data, 'Aa0.', 50, '|:,');
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 // TESTING
-function test_0(test_name, test_func, test_data, expected_result) {
+function test(test_type, test_name, test_func, test_data, p, expected_result) {
 
     test_data.pointer = 0;
 
@@ -75,8 +93,16 @@ function test_0(test_name, test_func, test_data, expected_result) {
     let len = test_data.string.length;
     while (i < len) {
         if (test_data.pointer >= len) { break; }
-        if (test_func(test_data)) { result += '1'; }
-        else { result += '0'; test_data.pointer += 1; }
+        switch (test_type) {
+            case 0: if (test_func(test_data)) { result += '1'; i += 1; continue; } break;
+            case 1: if (test_func(test_data,p[0])) { result += '1'; i += 1; continue; } break;
+            case 2: if (test_func(test_data,p[0],p[1])) { result += '1'; i += 1; continue; } break;
+            case 3: if (test_func(test_data,p[0],p[1],p[2])) { result += '1'; i += 1; continue; } break;
+            case 4: if (test_func(test_data,p[0],p[1],p[2],p[3])) { result += '1'; i += 1; continue; } break;
+            case 5: if (test_func(test_data,p[0],p[1],p[2],p[3],p[4])) { result += '1'; i += 1; continue; } break;
+        }
+        result += '0'; 
+        test_data.pointer += 1; 
         i += 1;
     }
 
@@ -106,12 +132,10 @@ let lower_expected = '0000000000000000000000000011111111111111111111111111000000
 let upper_expected = '11111111111111111111111111000000000000000000000000000000000000';
 
 
-test_0('digit', digit, data, digit_expected);
-test_0('lower', lower, data, lower_expected);
-test_0('upper', upper, data, upper_expected);
+test(0, 'digit', digit, data, null, digit_expected);
+test(0, 'lower', lower, data, null, lower_expected);
+test(0, 'upper', upper, data, null, upper_expected);
 
-
-let static_subset_expected = '100100100';
-
-test_1('static_subset', static_subset, data, static_subset_expected);
+let static_string_expected = '100000000000000000000000000000000000000000000000000000000000';
+test(1, 'static_string', static_string, data, ['ABC'], static_string_expected);
 
