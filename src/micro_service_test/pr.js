abc19d7763858527c6fdb90368190d5b7529217e
emanuel-alvgard <emanuel.alvgard@gmail.com>
2021-09-23 10:16:29 +0200

    -

--- src/micro_service_test/pr.js
+++ src/micro_service_test/pr.js
@@ -4,13 +4,16 @@ let net = require('net');
 
 let connection_data = {
     connected: 0,
-    id: 0
+    id: ''
 }
 
 let request_data = {
     s: '',
-    get string() { return this.s; },
     p: 0,
+
+    get string() { return this.s; },
+    set string(x) { this.s = x; },
+
     get pointer() { return this.p; },
     set pointer(x) { this.p = x; }    
 }
@@ -25,53 +28,59 @@ let response_data = {
 
 
 // EXTERNAL ACTIONS (called by other)
-function authenticate(conn, req) {
+function authenticate(con, req) {
     
     if (!parsing.static_string(req, 'id')) { return; }
     if (!parsing.symbol(req, ':')) { return; }
     let checkpoint = req.pointer;
     if (!parsing.dynamic_string(req, '0', 10, '|:,')) { return; }
-    req.pointer -= checkpoint;
 
-    conn.connected = 1;
-    conn.id = parsing.integer(req);
+    con.connected = 1;
+    con.id = req.string.substr(checkpoint, req.pointer);
     return;
 }
 
 
+function disconnect() {}
 
 
 
 
 // INTERNAL ACTIONS (called by self)
-function recieve(inc, conn, req, res) {
-    data_object.string = data.toString();
+function recieve(con, req, res) {
 
-    if (connected === 1) {
-        let len = data_object.string.length;
-        let i = 0;
-        while (i < len) {
-            // check for actions and execute
+    req.pointer = 0;
+
+    if (con.connected === 1) {
+
+        let len = req.string.length;
+        while (req.pointer < len) {
+            if (!parsing.static_string(req, 'action')) { return; }
+            if (!parsing.symbol(req, ':')) { return; }
+
+            if (parsing.static_string(req, 'disconnect')) { /* execute disconnect(); */}
         }
-        data_object.pointer = 0;
         return;
     }
-    if (parsing.static_string(data_object, 'authenticate')) {
-        authenticate(data_object);
-        data_object.pointer = 0;
-        return;
-    }
+
+    if (!parsing.static_string(req, 'action')) { return; }
+    if (!parsing.symbol(req, ':')) { return; }
+    if (!parsing.static_string(req, 'authenticate')) { return; }
+
+    authenticate(con, req);
+    return;
 }
 
 function send() {}
 
 
-function connect(client) {
+function connect(cli, con, req, res) {
     
-    client.write('action:authenticate,key:###');
+    // client.write('action:authenticate,key:###');
 
-    client.on('data', function(data) {
-       recieve(data);
+    cli.on('data', function(data) {
+       req.string = data.toString(); 
+       recieve(con, req, res);
     });
 }
 
@@ -82,7 +91,7 @@ function connect(client) {
 let options = { port: 3000 };
 
 let client = net.createConnection(options, function() {
-    connect(client);
+    connect(client, connection_data, request_data, response_data);
 });
 
 
