54c481e3eb6d5a8b5f7fac633ccd7e1dc16151df
emanuel-alvgard <emanuel.alvgard@gmail.com>
2021-09-26 21:36:13 +0200

    -

--- src/micro_service_test/pr.js
+++ src/micro_service_test/pr.js
@@ -3,17 +3,16 @@ let parsing = require('../parsing.js');
 let net = require('net');
 
 
-
 // EXTERNAL ACTIONS (called by other)
-function authenticate(cli, req) {
+function authenticate(client) {
     
-    if (!parsing.static_string(req, 'id')) { return; }
-    if (!parsing.symbol(req, ':')) { return; }
-    let checkpoint = req.pointer;
-    if (!parsing.dynamic_string(req, '0', 10, '|:,')) { return; }
+    if (!parsing.static_string(client, 'id')) { return; }
+    if (!parsing.symbol(client, ':')) { return; }
+    let checkpoint = client.pointer;
+    if (!parsing.dynamic_string(client, '0', 10, '|:,')) { return; }
 
-    cli.connected = 1;
-    cli.id = req.string.substr(checkpoint, req.pointer);
+    client.connected = 1;
+    client.id = client.string.substr(checkpoint, client.pointer);
     return;
 }
 
@@ -24,7 +23,7 @@ function disconnect() {}
 
 
 // INTERNAL ACTIONS (called by self)
-function recieve(cli, req, res) {
+function router(cli, req, res) {
 
     req.pointer = 0;
 
@@ -53,7 +52,7 @@ function recieve(cli, req, res) {
 
 
 
-function send(cli, data) {
+function message(cli, data) {
     if (cli.connected === 1) {
         cli.socket.write('|id:' + cli.id + data);
         return;
@@ -67,11 +66,11 @@ function send(cli, data) {
 
 function connect(cli, req, res) {
     
-    send(cli, '|action:authenticate,key:' + con.key);
+    message(cli, '|action:authenticate,key:' + con.key);
 
     cli.socket.on('data', function(data) {
        req.string = data.toString(); 
-       recieve(cli, req, res);
+       router(cli, req, res);
     });
 }
 
@@ -80,48 +79,63 @@ function connect(cli, req, res) {
 function start(address, port) {
 
     let client = {
-        socket: 0,
-        key: '###',
-        connected: 0,
-        id: ''
-    };
-    
-    // make all 3 into 1 object??
-    let request = {
-        s: '',
-        p: 0,
-    
-        get string() { return this.s; },
-        set string(x) { this.s = x; },
-    
-        get pointer() { return this.p; },
-        set pointer(x) { this.p = x; }    
-    };
-    
-    let response = {
-        string: ''
-    };
+        
+        // GENERAL
+        _socket: 0,
+        _connected: 0,
+        _key: '###',
+        _id: '',
 
+        get socket() { return this._socket; },
+        get connected() { return this._connected; },
+        get key() { return this._key; },
+        get id() { return this._id; },
 
+        set socket(value) { this._socket = value; },
+        set connected(value) { this._connected = value; },
+        set key(value) { this._key = value; },
+        set id(value) { this._id = value; },
+        
+        // REQUEST
+        _string: '',
+        _pointer: 0,
+    
+        get string() { return this._string; },
+        get pointer() { return this._pointer; },
+
+        set pointer(value) { this._pointer = value; },   
+        set string(value) { this._string = value; },
+
+        // ACTION
+        actions = [],
+
+        get actions() { return this.actions; },
+        set actions(value) { this.actions = value; }
+    }
+  
+
+    // CREATE SOCKET
     let options = { address: address, port: port };
 
     client.socket = net.createConnection(options, function() {
         connect(client, request, response);
     });
 
-    let result = {
-        client: client,
-        request: request,
-        response: response
-    };
-
-    return result;
+    return client;
 }
+
+
+function stop() {}
 
 
 router = start('127.0.0.1', 3000);
 
-send(router.client, '|action:log,data:hello from client!');
+function archive() {}
+
+action_add(router, 'archive', archive);
+action_remove(router, 'archive');
+
+send(router, '|action:log,data:hello from client!');
 
 
 // SPECIFIC FUNCTIONALITY WITH SENDS.
